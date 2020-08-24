e1ed844940468963c110a378c80d9ed9b9994b7b
Arif <agafizov@gmail.com>
2020-08-24 22:02:57 +0300

    Add SignUpForm

--- jumanji_v2/jumanji/jumanji/forms.py
+++ jumanji_v2/jumanji/jumanji/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.forms import Select
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from app_jumanji.models import Application, Company, Vacancy, Resume
 
@@ -27,3 +29,9 @@ class ResumeForm(forms.ModelForm):
     class Meta:
         model = Resume
         exclude = ['user']
+
+
+class SignUpForm(UserCreationForm):
+  class Meta:
+    model = User
+    fields = ('username', 'first_name', 'last_name')
