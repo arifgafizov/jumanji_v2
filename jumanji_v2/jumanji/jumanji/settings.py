360b26abbb4e39eabf9fc70266ffd76766eac5a7
Arif <agafizov@gmail.com>
2020-09-19 12:43:05 +0300

    Update settings.py

--- jumanji_v2/jumanji/jumanji/settings.py
+++ jumanji_v2/jumanji/jumanji/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd35nl9ihhdv(hx!ei01d&ow&ocrnv#sb=22$c5qk#jt6e=y3wr'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
