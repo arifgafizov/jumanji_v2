10fde456e1b8d3e222864c72d8bfa2a64fc1bccd
Arif <agafizov@gmail.com>
2020-09-19 14:43:22 +0300

    Update settings.py

--- jumanji_v2/jumanji/jumanji/settings.py
+++ jumanji_v2/jumanji/jumanji/settings.py
@@ -119,12 +119,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = ['app_jumanji/static',]
-
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/media/'
 
